480b36c31b3ab2e8b310447f16a253cd9369cd30
Brian Lee <brmlee@umich.edu>
2024-06-28 21:18:28 -0400

    working API

---
+++ FetchAssesment/MealDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  MealDetailView.swift
+//  FetchAssesment
+//
+//  Created by Brian Lee on 6/28/24.
+//
+
+import Foundation
---
+++ FetchAssesment/ViewModels.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModels.swift
+//  FetchAssesment
+//
+//  Created by Brian Lee on 6/28/24.
+//
+
+import Foundation
